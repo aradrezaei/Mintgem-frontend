0ddc66d56289efbdab76fb58c29d4549a894f812
Aradrezaei <aradrezaee50@gmail.com>
2025-04-28 22:46:13 +0330

    fix font

--- pages/_document.js
+++ pages/_document.js
@@ -1,23 +1,19 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document';
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="fa">
-        <Head>
-          <link
-            href="https://cdn.jsdelivr.net/gh/rastikerdar/shabnam-font@v5.0.1/dist/font-face.css"
-            rel="stylesheet"
-            type="text/css"
-          />
-        </Head>
-        <body className="font-shabnam">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html lang="fa" dir="rtl">
+      <Head>
+        <link
+          href="https://cdn.jsdelivr.net/gh/rastikerdar/shabnam-font@v5.0.1/dist/font-face.css"
+          rel="stylesheet"
+          type="text/css"
+        />
+      </Head>
+      <body className="font-shabnam bg-white dark:bg-darkbg transition-colors">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default MyDocument;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,17 +6,15 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        shabnam: ['Shabnam', 'sans-serif'],
+      },
       colors: {
         primary: '#6D28D9',   // بنفش سلطنتی
         secondary: '#9333EA', // بنفش پررنگ‌تر
         accent: '#F472B6',    // صورتی شیک
         darkbg: 'rgba(0,0,0,0.7)', // بک گراند دارک مود
-      },
-      
-      
-      fontFamily: {
-        sans: ['Shabnam', 'sans-serif'],
-      },
+      }
     },
   },
   plugins: [],
