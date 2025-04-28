40bcaa7c4f642a29f3d1b3c1fcdc3d010b37fd94
Aradrezaei <aradrezaee50@gmail.com>
2025-04-28 21:55:32 +0330

    rtl

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,12 @@ import '../styles/globals.css'
 export default function App({ Component, pageProps }) {
   return (
     <>
+        <html lang="fa" dir="rtl">
       <Head>
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/rastikerdar/shabnam-font@v5.0.1/dist/font-face.css" />
       </Head>
       <Component {...pageProps} />
+      </html>
     </>
   )
 }
