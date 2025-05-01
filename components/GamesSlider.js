d67ef5008b35b9973553a61fb3ba8176571c3b1b
Aradrezaei <aradrezaee50@gmail.com>
2025-05-01 16:40:37 +0330

    fix game slider

--- components/GamesSlider.js
+++ components/GamesSlider.js
@@ -7,7 +7,7 @@ import 'swiper/css';
 export default function GamesSlider() {
   const games = [
     { name: "Free Fire", image: "https://www.cdnlogo.com/logos/f/51/freefire-1.svg" },
-    { name: "Call of Duty Mobile", image: "/https://www.citypng.com/public/uploads/preview/hd-call-of-duty-mobile-codm-game-official-logo-png-7017516947877289uky0ombog.png" },
+    { name: "Call of Duty Mobile", image: "https://www.citypng.com/public/uploads/preview/hd-call-of-duty-mobile-codm-game-official-logo-png-7017516947877289uky0ombog.png" },
     { name: "Mobile Legends", image: "/images/mlbb-logo.png" },
     { name: "Clash of Clans", image: "/images/clash-logo.png" },
     { name: "Zepto", image: "/images/zepto-logo.png" },
@@ -16,15 +16,15 @@ export default function GamesSlider() {
 
   return (
     <section className="py-12 bg-white dark:bg-gray-900">
-      <div className="max-w-6xl mx-auto px-4">
+      <div className="max-w-7xl mx-auto px-4">
         <Swiper
           spaceBetween={20}
-          slidesPerView={2}
+          slidesPerView={1}
           autoplay={{ delay: 2500, disableOnInteraction: false }}
           breakpoints={{
-            640: { slidesPerView: 2 },
-            768: { slidesPerView: 3 },
-            1024: { slidesPerView: 6 },
+            640: { slidesPerView: 1 },
+            768: { slidesPerView: 2 },
+            1024: { slidesPerView: 4 },
           }}
           modules={[Autoplay]}
           className="w-full"
@@ -35,7 +35,7 @@ export default function GamesSlider() {
                 <img
                   src={game.image}
                   alt={game.name}
-                  className="h-20 object-contain transition hover:scale-110 duration-300 drop-shadow-md dark:drop-shadow-lg"
+                  className="h-24 w-auto object-contain transition hover:scale-110 duration-300 drop-shadow-md dark:drop-shadow-lg"
                 />
               </div>
             </SwiperSlide>
