db814b3cdd094f6443db81517fc2d115a00bb367
Aradrezaei <aradrezaee50@gmail.com>
2025-05-05 15:32:21 +0330

    fixing img

--- components/WhyMintGem.js
+++ components/WhyMintGem.js
@@ -5,7 +5,7 @@ export default function WhyMintGem() {
     <section className="py-24 bg-gradient-to-br from-white via-gray-50 to-white dark:from-gray-900 dark:via-gray-800 dark:to-gray-900">
       <div className="max-w-7xl mx-auto px-6 grid grid-cols-1 md:grid-cols-2 items-center gap-14">
         <div>
-          <h2 className="text-4xl md:text-5xl font-extrabold text-gray-800 dark:text-white mb-6">چرا مینت جم</h2>
+          <h2 className="text-4xl md:text-5xl font-extrabold text-gray-800 dark:text-white mb-6">درباره مینت جم</h2>
           <p className="text-gray-700 dark:text-gray-300 text-lg leading-relaxed">
             در دنیای شگفت‌انگیز بازی‌های دیجیتال، مینت جم با ارائه‌ی بهترین خدمات و تجربه خرید متفاوت، 
             مسیر خرید جم و ارزهای درون‌برنامه‌ای را برای گیمرهای ایرانی هموار کرده است. از خرید آنی و تحویل فوری گرفته تا 
@@ -16,7 +16,7 @@ export default function WhyMintGem() {
         <div className="w-full">
           <div className="overflow-hidden rounded-xl shadow-2xl border dark:border-gray-700">
             <img
-              src="https://sdmntprsouthcentralus.oaiusercontent.com/files/00000000-3f70-61f7-90e7-c90b80142ec4/raw?se=2025-05-05T09%3A07%3A59Z&sp=r&sv=2024-08-04&sr=b&scid=26f8454c-0877-5bb7-be3d-c9783b14d48d&skoid=cbbaa726-4a2e-4147-932c-56e6e553f073&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-05-04T22%3A55%3A34Z&ske=2025-05-05T22%3A55%3A34Z&sks=b&skv=2024-08-04&sig=HaafJybdK8upTVF%2BhULEP2r0nfkvOdd8WnjH52m7qhc%3D"
+              src="banner-mintgem.webp"
               alt="چرا MintGem"
               className="w-full h-80 object-cover hover:scale-105 transition duration-500 ease-in-out"
             />
