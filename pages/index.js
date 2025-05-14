ef19bf9bd206cd83641c7a850cc7a3904f8d8bca
Aradrezaei <aradrezaee50@gmail.com>
2025-05-14 07:52:07 +0330

    fix: disable ssr for swiper sliders

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
 'use client';
 
+import dynamic from 'next/dynamic';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import WhyMintGem from '../components/WhyMintGem';
 import FeaturesSection from '../components/FeaturesSection';
 import AmazingOffers from '../components/AmazingOffers';
+import ArticlesSlider from '../components/ArticlesSlider';
 import SocialMediaSection from '../components/SocialMediaSection';
 
-// ⚠️ Swiper کامپوننت‌ها فقط در کلاینت
-import dynamic from 'next/dynamic';
+// فقط اینا داینامیک باشن!
 const HeroSlider = dynamic(() => import('../components/HeroSlider'), { ssr: false });
 const GamesSlider = dynamic(() => import('../components/GamesSlider'), { ssr: false });
-const ArticlesSlider = dynamic(() => import('../components/ArticlesSlider'), { ssr: false });
 
 export default function Home() {
   return (
