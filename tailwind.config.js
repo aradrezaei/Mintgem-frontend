fcdcd057287e97ba2797d396f524cda30e5a6fb4
Aradrezaei <aradrezaee50@gmail.com>
2025-07-09 19:28:41 +0330

    fix  font

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,8 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        shabnam: ['Shabnam', 'sans-serif'],
-      },
+        sans: ['Shabnam', 'ui-sans-serif', 'system-ui'],
+            },
       colors: {
         primary: '#6D28D9',   // بنفش سلطنتی
         secondary: '#9333EA', // بنفش پررنگ‌تر
