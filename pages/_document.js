f9fe79e25f705d50a0d55f4d9679dc46ced6ec5d
Aradrezaei <aradrezaee50@gmail.com>
2025-07-12 10:59:14 +0330

    fix: add jsconfig for alias support

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="fa" dir="rtl" className={shabnam.variable}>
+    <Html lang="fa" dir="rtl" >
       <Head>
 
         <link rel="icon" href="/favicon.ico" />
